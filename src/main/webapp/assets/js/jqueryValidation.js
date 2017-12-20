831062ba38a2c3de06f3b03e193aab87cff56de0
Ihor Zhazhkyi <Ihor_Zhazhkyi@epam.com>
2017-12-20 15:42:19 +0200

    Task_10: added names and repeat password for validation

--- src/main/webapp/assets/js/jqueryValidation.js
+++ src/main/webapp/assets/js/jqueryValidation.js
@@ -1,26 +1,38 @@
-const REGEX_LOGIN = new RegExp("(\\w){3,16}");
+const REGEX_NAME = new RegExp("([a-zA-Zа-яА-Яё]){2,64}");
 const REGEX_EMAIL = new RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$");
 const REGEX_PASSWORD = new RegExp("(\\w|\\d){5,16}");
-const LOGIN_SELECTOR = '#login';
+const NAME_SELECTOR = '#name';
+const SURNAME_SELECTOR = '#surname';
 const EMAIL_SELECTOR = '#email';
 const PASSWORD_SELECTOR = '#password';
+const PASSWORD_REPEAT_SELECTOR = "#passwordRepeat";
 
 $(document).ready(function() {
 
-    var loginInput = $(LOGIN_SELECTOR);
+    var nameInput = $(NAME_SELECTOR);
+    var surnameInput = $(SURNAME_SELECTOR);
     var emailInput = $(EMAIL_SELECTOR);
     var passwordInput = $(PASSWORD_SELECTOR);
+    var passwordRepeatInput = $(PASSWORD_REPEAT_SELECTOR);
 
     $(document).on('submit', "#registrationForm", function(){
-            var login = loginInput.val();
+            var name = nameInput.val();
+            var surname = surnameInput.val();
             var email = emailInput.val();
             var password = passwordInput.val();
+            var passwordRepeat = passwordRepeatInput.val();
             var result = true;
-            $("#loginErr").hide();
+            $("#nameErr").hide();
+            $("#surnameErr").hide();
             $("#emailErr").hide();
             $("#passwordErr").hide();
-            if(!REGEX_LOGIN.test(login)){
-                $("#loginErr").show();
+            $("#passwprdRepeatErr")
+            if(!REGEX_NAME.test(name)){
+                $("#nameErr").show();
+                result = false;
+            }
+            if(!REGEX_NAME.test(surname)){
+                $("#surnameErr").show();
                 result = false;
             }
             if(!REGEX_EMAIL.test(email)){
@@ -29,6 +41,10 @@ $(document).ready(function() {
             }
             if(!REGEX_PASSWORD.test(password)){
                 $("#passwordErr").show();
+                result = false;
+            }
+            if(!(password === passwordRepeat)){
+                $("#passwordRepeatErr").show();
                 result = false;
             }
             return result;
